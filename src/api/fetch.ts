ae64f4363a58c07659df294a63247d4e8cde0868
jindw <jindaweihd@163.com>
2022-04-09 22:59:37 +0800

    feat:更新描述

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -7,7 +7,7 @@ function Get(url) {
         localStorage.setItem(url, JSON.stringify(data));
       })
       .catch(() => {
-        resolve(JSON.parse(localStorage.getItem(url) || "{}")); // 这里做了个缓存,在页面打开,断网不刷新生效,但因为有service worker 这里并不会生效了
+        resolve(JSON.parse(localStorage.getItem(url) || "{}")); // 这里做了个缓存,在页面打开,断网不刷新生效
       });
   });
 }
