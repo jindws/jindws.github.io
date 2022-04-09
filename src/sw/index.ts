dfab8d509c74cdece67f3589c4d721e15e35e8df
jindw <jindaweihd@163.com>
2022-04-09 14:05:21 +0800

    sw test3

---
+++ src/sw/index.ts
@@ -0,0 +1,13 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction) {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./dist/sw.js").then((res) => {
+      console.log(res);
+    });
+  }
+} else {
+  navigator.serviceWorker.register("./sw.js").then((res) => {
+    console.log(res);
+  });
+}
